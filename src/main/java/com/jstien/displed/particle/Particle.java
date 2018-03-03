60389dafa7d5681a0c3ac954157a8fa61f343416
Joakim Stien <joakim.stien@gmail.com>
2018-03-03 22:24:06 +0100

    Started work on redoing the particle system

--- src/main/java/com/jstien/displed/particle/Particle.java
+++ src/main/java/com/jstien/displed/particle/Particle.java
@@ -4,18 +4,10 @@ import java.awt.*;
 
 public class Particle {
     public Particle(float posX, float posY) {
-        color = new Color(
-                (int)(Math.random()*20f + 10f),
-                (int)(Math.random()*20f + 10f),
-                (int)(Math.random()*20f + 10f),
-                (int)(Math.random()*30f + 20f)
-        );
-
         position = new Vector2(posX, posY);
         target = new Vector2(position);
     }
 
     Vector2 position;
     Vector2 target;
-    Color color;
 }
